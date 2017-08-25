a82079b7fd32d06b95fe75e7ae2867b17a0fb3e3
Jeremy Zahner <zahner@joshmartin.ch>
2017-08-25 14:21:41 +0100

    Adds basic tests.

--- __tests__/app.js
+++ __tests__/app.js
@@ -3,7 +3,8 @@ var path = require('path');
 var assert = require('yeoman-assert');
 var helpers = require('yeoman-test');
 
-describe('generator-jm-vuejs:app', () => {
+describe('generator-jm-vuejs:app | Basic Usage', () => {
+
   beforeAll(() => {
     return helpers.run(path.join(__dirname, '../generators/app'))
       .withPrompts({
@@ -15,14 +16,86 @@ describe('generator-jm-vuejs:app', () => {
         serviceworker: true,
         browserconfig: true,
         manifest: true,
-        vuejsComponents: [
-          'routing',
-          'stateManagement',
-          'translations',
-          'httpClient',
-          'graphqlClient',
-          'dependencyInjection',
-        ]
+        vuejsComponents: []
       });
   });
+
+  it('creates all "simpleFiles" files', () => {
+    assert.file([
+      '.csscomb.json',
+      '.editorconfig',
+      '.eslintignore',
+      '.eslintrc.json',
+      '.gitattributes',
+      '.gitignore',
+      '.htmlhintrc',
+      '.modernizrrc.json',
+      'jsconfig.json',
+      'README.md',
+      'bin/dev-server',
+      'bin/setup-client-config',
+      'src/assets/.gitkeep',
+      'src/helpers/.gitkeep',
+      'src/mixins/.gitkeep',
+      'src/plugins/.gitkeep',
+      'src/styles/fonts.scss',
+      'src/styles/variables.scss',
+      'src/global.scss',
+    ]);
+  });
+
+  it('creates all "tplFiles" files', () => {
+    assert.file([
+      '.nvmrc',
+      'src/client.config.dist.json',
+      'src/main.js',
+      'src/index.html',
+      'src/components/app',
+    ]);
+  });
+
+  it('creates all "tplFiles" files', () => {
+    assert.file([
+      '.nvmrc',
+      'src/client.config.dist.json',
+      'src/main.js',
+      'src/index.html',
+      'src/components/app',
+    ]);
+  });
+
+  it('creates all "ciFiles" files', () => {
+    assert.file([
+      'circle.yml',
+    ]);
+  });
+
+  it('creates all "serviceworkerFiles" files', () => {
+    assert.file([
+      'src/sw.js',
+    ]);
+  });
+
+  it('creates all "bundlerFiles" files', () => {
+    assert.file([
+      'webpack/base.js',
+      'webpack/dev.js',
+      'webpack/prod.js',
+      'webpack/server.conf.js',
+      'webpack/server.js',
+    ]);
+  });
+
+  it('creates all "browserconfigFiles" files', () => {
+    assert.file([
+      'src/browserconfig.xml',
+    ]);
+  });
+
+  it('creates all "manifestFiles" files', () => {
+    assert.file([
+      'src/manifest.json',
+    ]);
+  });
+
 });
