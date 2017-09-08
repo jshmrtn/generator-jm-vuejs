3a8cc04a740319c4925396409d9453be214b54d9
Jeremy Zahner <zahner@joshmartin.ch>
2017-09-08 19:10:02 +0200

    Adds basic graphQL configuration.

--- generators/app/templates/src/core/graphql/_index.js
+++ generators/app/templates/src/core/graphql/_index.js
@@ -1,24 +1,22 @@
 import Vue from 'vue';
-import { ApolloClient, createBatchingNetworkInterface } from 'apollo-client';
+import { ApolloClient, createNetworkInterface } from 'apollo-client';
 import VueApollo from 'vue-apollo';
+import { graphqlRoot } from 'src/client.config.json';
 
 // Create the apollo client
 export const apolloClient = new ApolloClient({
   networkInterface: createBatchingNetworkInterface({
-    uri: 'http://localhost:3020/graphql',
+    uri: graphqlRoot,
   }),
   connectToDevTools: true,
 });
 
 // Install the vue plugin
-Vue.use(VueApollo)
+Vue.use(VueApollo, { apolloClient });
 
 // Add provider
 const apolloProvider = new VueApollo({
   defaultClient: apolloClient,
-  defaultOptions: {
-    // $loadingKey: 'loading',
-  },
   errorHandler (error) {
     console.error(error);
   },
